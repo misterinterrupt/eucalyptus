cce176fbe45ac97ca2d07fbf790bd9c313fb8448
Matt Howell <misterinterrupt@gmail.com>
2011-04-28 15:14:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eucalyptus::Application.config.secret_token = '382441054ed78ce95968cd8160710a5436010207d9638101de1d2126a265d6acbd2dfd0bf4c4b4f15654549f9ebe1f8c211a79e7fb6c8e0aab62f5a5232176bf'
