170fad73b3d996b6364698ae92c44a19b8a571ad
Matt Howell <misterinterrupt@gmail.com>
2011-04-28 16:47:12 -0700

    added the partyfreakoutonline controller, view and route

---
+++ app/controllers/partyfreakoutonline_controller.rb
@@ -0,0 +1,11 @@
+class PartyfreakoutonlineController < ApplicationController
+  
+  # GET /partyfreakoutonline
+  def index
+    
+    respond_to do |format|
+      format.html
+    end
+    
+  end
+end
